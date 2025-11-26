92c17cddd308253d246d55ebbdce58a40cd41cdb
gitAccountTestingTime <133053355+gitAccountTestingTime@users.noreply.github.com>
2025-11-26 14:56:25 -0500

    Remove output quantity input and related code
    Removed input fields and related initialization logic from the main application.

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,7 @@ import {createMap, createOutputSelect, initOutputQuantity} from './recipe-map'
 document.querySelector('#app').innerHTML = `
 <div>
     <p>
-        Output Quantity/min:
-        <input type="number" id="outputQuantity" value="10" min="1">
-    </p>
-    <p>
-        Output: <select id="materials"></select>
-    </p>
-  
-    <p>
-        Recipes
-        <div id="recipes"></div>
+        We Alive?
     </p>
 </div>
-`
 
-createOutputSelect();
-initOutputQuantity();
-createMap();
-
-const select = document.getElementById("materials") as HTMLSelectElement;
-
-const lastSelection = localStorage.getItem("last-material") || "IRON_ROD";
-const option = select.options.namedItem(lastSelection);
-if (option) {
-    option.selected = true;
-    select.dispatchEvent(new Event("change"));
-}
